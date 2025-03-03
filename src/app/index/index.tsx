40edbf0911a3af467bf63ee0e8b266718d0d1ee6
Fridson Firmino <fridsoncmf@gmail.com>
2025-03-03 12:23:23 +0100

    feat: I created the navigation strategy, the header and the category component

---
+++ src/app/index/index.tsx
@@ -0,0 +1,26 @@
+import { View, Image, TouchableOpacity } from "react-native";
+import { MaterialIcons } from "@expo/vector-icons";
+
+import { styles } from "@/app/index/style";
+import { colors } from "@/styles/colors";
+
+import { Category } from "@/components/category";
+
+export default function Index() {
+  return (
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <Image source={require("@/assets/logo.png")} style={styles.logo} />
+
+        <TouchableOpacity activeOpacity={0.6} style={styles.btnAdd}>
+          <MaterialIcons name="add" size={30} color={colors.gray[950]} />
+        </TouchableOpacity>
+      </View>
+
+      {/* Categories */}
+      <Category name="Projecto" icon="code" isSelected={true} />
+      <Category name="Site" icon="language" isSelected={false} />
+      <Category name="Video" icon="movie" isSelected={false} />
+    </View>
+  );
+}
---
+++ src/app/index/style.ts
@@ -0,0 +1,30 @@
+import { StyleSheet } from "react-native";
+
+import { colors } from "@/styles/colors";
+
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop: 24,
+  },
+  header: {
+    width: "100%",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    paddingHorizontal: 24,
+    marginBottom: 24,
+  },
+  logo: {
+    width: 38,
+    height: 34,
+  },
+  btnAdd: {
+    width: 36,
+    height: 32,
+    backgroundColor: colors.green[300],
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 5,
+  },
+});
